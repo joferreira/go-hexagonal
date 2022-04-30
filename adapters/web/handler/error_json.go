1261acb767cceb273205b715f65f799fe09324f0
joferreira <jf.sorin@gmail.com>
2022-04-30 15:57:51 -0300

    Finalizado

---
+++ adapters/web/handler/error_json.go
@@ -0,0 +1,16 @@
+package handler
+
+import "encoding/json"
+
+func jsonError(msg string) []byte {
+	errr := struct {
+		Message string `json:"message"`
+	}{
+		Message: msg,
+	}
+	r, err := json.Marshal(errr)
+	if err != nil {
+		return []byte(err.Error())
+	}
+	return r
+}
--- application/product_test.go
+++ application/product_test.go
@@ -1,7 +1,8 @@
-package application
+package application_test
 
 import (
 	"github.com/joferreira/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -19,4 +20,41 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
 
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "invalid"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be either enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater than zero", err.Error())
 }
